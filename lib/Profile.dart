4167917880ffb93038808c33308ae39d604f7383
mahbubajebinmj <mahbubajebin.mj.24@gmail.com>
2024-11-13 04:15:49 +0600

    main edited

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -1,1 +1,69 @@
 import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({Key? key}) : super(key: key);
+
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(50.0))),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 40.0,
+                ),
+                ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  title: Text(
+                    "Hey joshan!",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall
+                        ?.copyWith(color: Colors.white),
+                  ),
+                  subtitle: Text(
+                    "GoodMorning",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(color: Colors.white54),
+                  ),
+                  trailing: const CircleAvatar(
+                    radius: 30.0,
+                    backgroundImage: AssetImage("images/profile.png"),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                )
+              ],
+            ),
+          ),
+          Container(
+            color: Theme.of(context).primaryColor,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(100),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
